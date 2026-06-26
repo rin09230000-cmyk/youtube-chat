fd77c84847928448b815cda89feac5e18809c0e8
rin09230000-cmyk <rin09230000@gmail.com>
2026-06-26 09:39:31 +0900

    Update 00_수익분석.py

--- pages/00_수익분석.py
+++ pages/00_수익분석.py
@@ -24,6 +24,10 @@ st.set_page_config(
 st.title("📊 유튜브 채널 수익 분석기")
 st.caption("채널명 또는 채널 URL을 입력하세요")
 
+channel_input = st.text_input("채널명 또는 URL 입력")
+
+if channel_input:
+    st.session_state["channel"] = channel_input
 # --------------------------
 # URL 처리
 # --------------------------
