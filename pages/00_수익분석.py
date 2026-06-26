61518f8eabe34c856086053f438228193505bb80
rin09230000-cmyk <rin09230000@gmail.com>
2026-06-26 09:00:41 +0900

    Update 00_수익분석.py

--- pages/00_수익분석.py
+++ pages/00_수익분석.py
@@ -1,6 +1,35 @@
 import streamlit as st
-from utils import get_channel_data
+from googleapiclient.discovery import build
 
+api_key = st.secrets["YOUTUBE_API_KEY"]
+
+youtube = build(
+    "youtube",
+    "v3",
+    developerKey=api_key
+)
+def get_channel_data(channel_id):
+
+    response = youtube.channels().list(
+        part="snippet,statistics",
+        id=channel_id
+    ).execute()
+
+    if not response["items"]:
+        return None
+
+    item = response["items"][0]
+
+    stats = item["statistics"]
+    snippet = item["snippet"]
+
+    return {
+        "title": snippet["title"],
+        "thumbnail": snippet["thumbnails"]["high"]["url"],
+        "subs": int(stats.get("subscriberCount", 0)),
+        "views": int(stats.get("viewCount", 0)),
+        "videos": int(stats.get("videoCount", 0))
+    }
 st.title("💰 수익 분석")
 
 if "channel_id" not in st.session_state:
